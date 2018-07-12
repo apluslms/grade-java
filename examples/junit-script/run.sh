3b33ddc3ef5be3c47f3ce64000ea0c27a358797d
Jaakko Kantojärvi <jaakko.kantojarvi@aalto.fi>
2018-07-12 14:26:31 +0300

    Refactor example script

--- examples/junit-script/run.sh
+++ examples/junit-script/run.sh
@@ -3,11 +3,9 @@
 files="HelloWorld.java /exercise/HelloWorldTest.java /exercise/AllTests.java"
 files=$(move-to-package-dirs -v $files)
 
-if capture pre java-compile -M; then
-    if capture pre junit -r AllTests; then
-        title -e p "Everything is ok and you got full points. Well done!"
-    fi
-else
-    title -e p "Cimpilation failed, no tests are run. Compilation errors:"
+if ! capture pre java-compile -M; then
+    title -e p "Compilation failed, no tests were run. Compilation errors:"
     err-to-out
+elif capture pre junit -r AllTests; then
+    title -e p "Everything is ok and you got full points. Well done!"
 fi
